a2e261e080e39a4fd3cf8305c28198a3a9f2df4e
pranavmanglani <sssfacts20@gmail.com>
2025-05-19 10:13:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+name = "Pranav Manglani"
+def change_name():
+    name = "changed"
+st.button("Change name",onclick=changename())
